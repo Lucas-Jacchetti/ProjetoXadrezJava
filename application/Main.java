4dd93d92d43b40f319452b5c7c1b1c7cb5d464bb
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2024-01-02 11:01:30 -0300

    Primeira classe, Position.java

---
+++ application/Main.java
@@ -0,0 +1,10 @@
+package chesSystem.application;
+
+import chesSystem.boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
