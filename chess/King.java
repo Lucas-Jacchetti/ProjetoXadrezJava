b71ecb4231d2961f3bf0c83a639d4dfdc649cb6b
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2024-01-02 11:53:01 -0300

    Colocando peças no tabuleiro(não todas)

---
+++ chess/King.java
@@ -0,0 +1,15 @@
+package chesSystem.chess;
+
+import chesSystem.boardgame.Board;
+
+public class King extends ChessPiece{
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+    @Override
+    public String toString() {
+        return "R";
+    }
+    
+}
